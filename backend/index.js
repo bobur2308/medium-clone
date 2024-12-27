1577f6b906a6ce8b96f7221c9dd502afd1be98e5
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-27 11:44:38 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const connectDB = require('./Config/db');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
 
 
 app.get('/', (req, res) => {
   res.send('<h1>Server is running</h1>' );
 })
+
 // routes
+app.use('/api/auth', require('./Routers/AuthRouter'));
 
-
+app.use('/api/profile',require('./Routers/UserRouter'))
 
 
 connectDB()
