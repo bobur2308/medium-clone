286af803d306ee50bab0cb2704efc2f55cdd3556
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-27 16:38:01 +0500

    Update UserRouter.js

--- backend/Routers/UserRouter.js
+++ backend/Routers/UserRouter.js
@@ -9,8 +9,7 @@ router.get('/user-info',authenticate,UserController.getUserInfo)
 // user-post-router
 router.get('/posts/all',authenticate,UserController.getAllPosts)
 router.post('/posts/add',authenticate,UserController.addNewPost)
-
-
-
+router.put('/posts/edit/:id',authenticate,UserController.editPostById)
+router.delete('/posts/delete/:id',authenticate,UserController.deletePostById)
 
 module.exports = router
