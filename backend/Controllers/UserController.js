cf6d1bd29f6763f39ece0f1c49c360ff186765f6
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-27 15:24:34 +0500

    updates

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,5 +1,25 @@
 const errorHandler = require("../Middlewares/ErrorMiddleware")
 const User = require("../Models/User")
+const path = require('path')
+const multer = require('multer');
+const Post = require("../Models/Post");
+
+
+const postStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = path.join(__dirname, "../uploads/post-files");
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const now = new Date();
+    const date = now.toISOString().split("T")[0]; // Format: YYYY-MM-DD
+    const timestamp = `${now.getHours()}${now.getMinutes()}${now.getSeconds()}`; // Format: HHMMSS
+    const fileName = `${date}-${timestamp}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+const postUpload = multer({ storage: postStorage });
+
 
 class UserController{
   async getUserInfo(req,res){
@@ -23,6 +43,35 @@ class UserController{
     } catch (error) {
       errorHandler(error,res)
     }
+  }
+  async addNewPost(req,res){
+    postUpload.single('image')(req,res, async (err)=>{
+      if (err) {
+        return res.status(400).json({ message: "File upload failed", error: err.message });
+      }
+      try {
+        const {
+          title,
+          description,
+        } = req.body
+        const filePath = `/uploads/post-files/${req.file.filename}`;
+        // console.log(req.user);
+
+        const data = await Post.create({
+          title,
+          description,
+          filePath,
+          authorId:req.user.userId
+        })
+        
+        res.status(200).json({
+          message:"Success",
+          data
+        })
+      } catch (error) {
+        errorHandler(error,res)
+      }
+    })
   }
   
 }
--- backend/Routers/UserRouter.js
+++ backend/Routers/UserRouter.js
@@ -2,8 +2,15 @@ const UserController = require('../Controllers/UserController')
 const authenticate = require('../Middlewares/AuthMiddleware')
 
 const router = require('express').Router()
-
+// user-profile-router
 router.get('/user-info',authenticate,UserController.getUserInfo)
+
+
+// user-post-router
 router.get('/posts/all',authenticate,UserController.getAllPosts)
+router.post('/posts/add',authenticate,UserController.addNewPost)
+
+
+
 
 module.exports = router
